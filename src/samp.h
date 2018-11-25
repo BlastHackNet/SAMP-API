bcbdbc744fc91212de628de3218366fa36cfb56c
LUCHARE <34620255+LUCHARE@users.noreply.github.com>
2018-11-25 16:51:02 +1000

    Create samp.h

---
+++ src/samp.h
@@ -0,0 +1,10 @@
+/*
+	This is a SAMP (0.3.7-R1) API project file.
+	Developer: LUCHARE <luchare.dev@gmail.com>
+	
+	See more here https://github.com/LUCHARE/SAMP-API
+	
+	Copyright (c) 2018 BlastHack Team <BlastHack.Net>. All rights reserved.
+*/
+
+#pragma once
